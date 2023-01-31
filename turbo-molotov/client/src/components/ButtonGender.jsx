821d981033437985d4f07992d63a900130b02df6
Jason <61055622+JayIrish@users.noreply.github.com>
2023-01-31 09:37:55 -0500

    Update ButtonGender.jsx

--- turbo-molotov/client/src/components/ButtonGender.jsx
+++ turbo-molotov/client/src/components/ButtonGender.jsx
@@ -17,7 +17,7 @@ export default function ButtonGender() {
       <FormControl fullWidth>
         <InputLabel id="demo-simple-select-label">Sexe</InputLabel>
         <Select
-        required
+          required
           labelId="demo-simple-select-label"
           id="demo-simple-select"
           value={sexe}
