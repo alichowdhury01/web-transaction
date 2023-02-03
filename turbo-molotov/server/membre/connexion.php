b8ca697cf14c51c52bd160f9f6ce0d0d8d95b87c
Jason <61055622+JayIrish@users.noreply.github.com>
2023-02-02 21:07:26 -0500

    Session_id added to response

--- turbo-molotov/server/membre/connexion.php
+++ turbo-molotov/server/membre/connexion.php
@@ -7,21 +7,25 @@
 
     function validerMDP(){
         global $connexion, $email, $password;
-        $requette = "SELECT pass, 'role' FROM connexion WHERE courriel = (?)";
+        $requette = "SELECT pass, `role` FROM connexion WHERE courriel = (?)";
         $stmt = $connexion->prepare($requette);
         $stmt->execute([$email]);
         while ($row = $stmt->fetch()) {
             if($row['pass'] === $password){
                 if($row['role'] === "M"){
                     session_start();
-                    echo("Vous etes connecté!", session_id());
+                    $reponse = array("reponse" => "Vous etes connecté en temps que membre!", "session" => session_id());
+                    echo json_encode($reponse);
                     header("Location: http://localhost:3000/membre");
                 }else if($row['role'] === "A"){
-                    echo("Vous etes connecté!",session_id());
                     session_start();
+                    $reponse = array("reponse" => "Vous etes connecté en temps qu'administrateur'!", "session" => session_id());
+                    echo json_encode($reponse);
                     header("Location: http://localhost:3000/admin");
+                }else{
+                    $reponse = array("Erreur de mot de passe!");
+                    echo json_encode($reponse);
                 }
-                
             }
         }
         exit();
@@ -32,16 +36,25 @@
         $requette = "SELECT courriel FROM connexion";
         $stmt = $connexion->prepare($requette);
         $stmt->execute();
+        $trouve = false;
         while ($row = $stmt->fetch()) {
-            if($row['courriel'] === $email ){
-                validerMDP();echo json_encode("email valide");
+            if($row['courriel'] !== $email ){
+                
             }
             else{
-                echo json_encode("email non-valide réessayer de nouveaux ");
+                $trouve = true;
+                break;
             }
+        }
+        if($trouve === false){
+            echo json_encode("email non-valide réessayer de nouveaux ");
+        }else{
+            validerMDP();
+            echo json_encode("email valide");
         }
         unset($connexion); //Detruire la connexion		
     }
+    
     validerEmail();
     exit();
 ?>
