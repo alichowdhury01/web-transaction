3b33cdfda44ec1161cd547d2acc90f702e297c3a
alichowdhury01 <alichowdhury01@gmail.com>
2023-02-03 20:13:43 -0500

    Update App.js

--- turbo-molotov/client/src/App.js
+++ turbo-molotov/client/src/App.js
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import {  Navbar2, Home, Magasiner, SignIn, SignUp, Connexion, Membre, ContactUs} from "./scenes/index";
+import {  Navbar2, Home, Magasiner, SignIn, SignUp, Connexion, Membre, ContactUs, Admin} from "./scenes/index";
+
+let routePath;
+let elementPath;
 const ScrollToTop = () => {
   const { pathname } = useLocation();
 
@@ -10,7 +13,15 @@ const ScrollToTop = () => {
 
   return null;
 };
-
+//à determiner le où et comment faire pour que le path soit dynamique 
+if (localStorage.getItem('role') === "M") { 
+  routePath = "/"+localStorage.getItem('session'); 
+  elementPath = <Membre />;
+}
+else if (localStorage.getItem('role') === "A") { 
+  routePath = "/"+localStorage.getItem('session');
+  elementPath = <Admin />;
+}
 function App() {
 
   return (
@@ -21,7 +32,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/signin" element={<Connexion />}/>
-          <Route path="/membre" element={<Membre />}/>
+          <Route path={routePath} element={elementPath}/>
           <Route path="/contact" element={<ContactUs />}/>
         </Routes>
       </BrowserRouter>
