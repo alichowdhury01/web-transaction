e6a267bdc0980a150fe289bffb4b75b0c8f345d7
alichowdhury01 <alichowdhury01@gmail.com>
2023-02-19 16:33:12 -0500

    Create controlleurArticle.php

---
+++ turbo-molotov/server/article/controlleurArticle.php
@@ -0,0 +1,42 @@
+<?php
+    header('Access-Control-Allow-Origin: http://localhost:3000');
+    header('Content-Type: application/json');
+    require_once('../includes/connexionBDD.inc.php');
+    
+    function createArticle(){
+        $nom = $_POST['nom'];
+        $categorie = $_POST['categorie'];
+        $prix = $_POST['prix'];
+        $description = $_POST['description'];
+        $quantiteInventaire = $_POST['quantiteInventaire'];
+
+        $repArticle = "../../client/src/assets/cardPicture/";
+        try{
+            if($_FILES['article']['tmp_name'] != ""){
+                $tmpFile = $_FILES['article']['tmp_name'];
+                $nomOriginal = $_FILES['avatars']['name'];
+                $extension = strrchr($nomOriginal,'.');
+                $nouveauNom = sha1($nom.time()).$extension;
+                @move_uploaded_file($tmpFile, $repArticle.$nouveauNom);
+            }  
+            global $connexion, $categorie, $nom, $description, $prix, $quantiteInventaire, $repArticle, $nouveauNom;
+            $requette = "INSERT INTO articles VALUES(0, ?, ?, ?, ?, ?, ?)";
+            $stmt = $connexion->prepare($requette);
+            $stmt->execute([$categorie, $nom, $description, $prix, $quantiteInventaire, $repArticle.$nouveauNom]);
+
+            $msg = array("status" => "OK","msg" => "Article $nom bien enregistre");
+            echo json_encode('ttes');
+            echo json_encode($msg['msg']);
+        }catch(PDOException $e){
+            $msg = array("status" => "KO","msg" => "Erreur d'enregistrement de l'article $nom");
+            echo json_encode($msg);
+        } finally {
+            unset($connexion); //Detruire la connexion
+        }
+
+                		
+    }
+        
+        // header("Location : localhost:3000");
+exit();
+?>
