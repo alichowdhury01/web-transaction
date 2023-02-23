4369196532c374f699871431ec01898012971cb5
alichowdhury01 <alichowdhury01@gmail.com>
2023-02-23 09:41:01 -0500

    Update MainCarousel.jsx

--- turbo-molotov/client/src/scenes/home/MainCarousel.jsx
+++ turbo-molotov/client/src/scenes/home/MainCarousel.jsx
@@ -1,11 +1,9 @@
-import { Box, Typography, IconButton, useMediaQuery } from "@mui/material";
+import { Box, IconButton, useMediaQuery } from "@mui/material";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import { shades } from "../../theme";
 
-// imports all images from assets folder
 const importAll = (r) =>
   r.keys().reduce((acc, item) => {
     acc[item.replace("./", "")] = r(item);
