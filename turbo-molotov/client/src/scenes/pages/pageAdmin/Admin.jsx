53c35d8e91e33b732d9da576c524195ea32d8cdf
Jason <61055622+JayIrish@users.noreply.github.com>
2023-03-04 10:09:40 -0500

    Update Admin.jsx

--- turbo-molotov/client/src/scenes/pages/pageAdmin/Admin.jsx
+++ turbo-molotov/client/src/scenes/pages/pageAdmin/Admin.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Box, Typography} from "@mui/material";
-
+import EnhancedTable from '../../../components/Table';
 const Admin = () => {
   return (
     <Box>
-        <Typography variant="h1">Admin</Typography>
+        <EnhancedTable />
     </Box>
   )
 }
