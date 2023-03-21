519c891628dd3b5c0f93b929738f714bf91ec838
Jason <61055622+JayIrish@users.noreply.github.com>
2023-03-21 10:12:33 -0400

    Update controleurMembre.php

--- turbo-molotov/server/membre/controleurMembre.php
+++ turbo-molotov/server/membre/controleurMembre.php
@@ -17,69 +17,97 @@
 
         function reqEnreg(){
                 global $connexion;
-                $firstName = $_POST['firstName'];
-                $lastName = $_POST['lastName'];
-                $gender = $_POST['gender'];
-                $dateNaissance = $_POST['dateNaissance'];
-                $email = $_POST['email'];
-                $password = $_POST['password'];
-                $requette = "INSERT INTO membres VALUES(0, ?, ?, ?, ?, ?)";
-                $stmt = $connexion->prepare($requette);
-                $stmt->execute([$firstName, $lastName, $email, $gender, $dateNaissance]);
+                try{
+                        $firstName = $_POST['firstName'];
+                        $lastName = $_POST['lastName'];
+                        $gender = $_POST['gender'];
+                        $dateNaissance = $_POST['dateNaissance'];
+                        $email = $_POST['email'];
+                        $password = $_POST['password'];
+                        $requette = "INSERT INTO membres VALUES(0, ?, ?, ?, ?, ?)";
+                        $stmt = $connexion->prepare($requette);
+                        $stmt->execute([$firstName, $lastName, $email, $gender, $dateNaissance]);
 
-                $requette = "SELECT id FROM membres WHERE courriel = (?)";
-                $stmt = $connexion->prepare($requette);
-                $stmt->execute([$email]);
-                $rs = $stmt->fetch();
-                $id = $rs["id"];
+                        $requette = "SELECT id FROM membres WHERE courriel = (?)";
+                        $stmt = $connexion->prepare($requette);
+                        $stmt->execute([$email]);
+                        $rs = $stmt->fetch();
+                        $id = $rs["id"];
 
-                $requette = "INSERT INTO connexion  VALUES(?, ?, ?, 'M', 'A')";
-                $stmt = $connexion->prepare($requette);
-                $stmt->execute([$id,$email, $password]);
-                
-                $msg = array("status" => "OK","msg" => "Membre $email bien enregistre");
-                echo json_encode($msg);
-                unset($connexion); //Detruire la connexion		
+                        $requette = "INSERT INTO connexion  VALUES(?, ?, ?, 'M', 'A')";
+                        $stmt = $connexion->prepare($requette);
+                        $stmt->execute([$id,$email, $password]);
+                        
+                        $msg = array("status" => "OK","msg" => "Membre $email bien enregistre");
+                        echo json_encode($msg);   
+                }catch(PDOException $e){
+                        $msg = array("status" => "KO","msg" => "Erreur de controleur enreg mambres");
+                        echo json_encode($msg);
+                    } finally {
+                        unset($connexion); //Detruire la connexion
+                    }
+                		
         }
 
         function reqDesActiv() {
                 global $connexion;
-                $id = $_POST['id'];
+                try{
+                        $id = $_POST['id'];
                 
-                $requette = "UPDATE connexion SET statut = 'I' WHERE id=(?)";
-                $stmt = $connexion->prepare($requette);
-                $stmt->execute([$id]);
+                        $requette = "UPDATE connexion SET statut = 'I' WHERE id=(?)";
+                        $stmt = $connexion->prepare($requette);
+                        $stmt->execute([$id]);
+                        
+                        $msg = array("status" => "OK","msg" => "Membre $id désactivé");
+                        echo json_encode($msg);    
+                }catch(PDOException $e){
+                        $msg = array("status" => "KO","msg" => "Erreur de controleur désactivation du member");
+                        echo json_encode($msg);
+                    } finally {
+                        unset($connexion); //Detruire la connexion
+                    }
                 
-                $msg = array("status" => "OK","msg" => "Membre $id désactivé");
-                echo json_encode($msg);
-                unset($connexion); //Detruire la connexion
         }
 
         function reqReActiv() {
                 global $connexion;
-                $id = $_POST['id'];
+                try{
+                       $id = $_POST['id'];
                 
-                $requette = "UPDATE connexion SET statut = 'A' WHERE id=(?)";
-                $stmt = $connexion->prepare($requette);
-                $stmt->execute([$id]);
+                        $requette = "UPDATE connexion SET statut = 'A' WHERE id=(?)";
+                        $stmt = $connexion->prepare($requette);
+                        $stmt->execute([$id]);
+                        
+                        $msg = array("status" => "OK","msg" => "Membre $id activé");
+                        echo json_encode($msg); 
+                }catch(PDOException $e){
+                        $msg = array("status" => "KO","msg" => "Erreur de controleur d'activation du member");
+                        echo json_encode($msg);
+                    } finally {
+                        unset($connexion); //Detruire la connexion
+                    }
                 
-                $msg = array("status" => "OK","msg" => "Membre $id activé");
-                echo json_encode($msg);
-                unset($connexion); //Detruire la connexion
         }
 
         function getAccountInfo() {
                 global $connexion;
                 $email = $_POST['email'];
+
+                try{
+                  $requette = "SELECT * FROM membres WHERE courriel=(?)";
+                        $stmt = $connexion->prepare($requette);
+                        $stmt->execute([$email]);
+                        $rs = $stmt->fetchAll();
+                        
+                        $msg = array("status" => "OK","msg" => $rs);
+                        echo json_encode($msg);   
+                }catch(PDOException $e){
+                        $msg = array("status" => "KO","msg" => "Erreur de controleur d'account du member");
+                        echo json_encode($msg);
+                    } finally {
+                        unset($connexion); //Detruire la connexion
+                    }
                 
-                $requette = "SELECT * FROM membres WHERE email=(?)";
-                $stmt = $connexion->prepare($requette);
-                $stmt->execute([$id]);
-                $rs = $stmt->fetch();
-                
-                $msg = array("status" => "OK","msg" => $rs);
-                echo json_encode($msg);
-                unset($connexion); //Detruire la connexion
         }
 
         $action = $_POST['action'];
@@ -95,6 +123,7 @@
                 break;
                 case 'getAccount':
                         getAccountInfo();
+                break;
                 default:
                 $msg = array("status" => "KO","msg" => "Erreur du controleur des membres");
                 echo json_encode($msg);
